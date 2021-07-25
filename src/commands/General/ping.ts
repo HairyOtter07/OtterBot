9b15847bdb7a4ddda8e26ed2849eeee3aa881258
HairyOtter07 <66044018+HairyOtter07@users.noreply.github.com>
2021-07-25 15:35:08 -0700

    refactor: add detailed description to ping command

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -4,7 +4,8 @@ import { Message } from "discord.js";
 
 @ApplyOptions<CommandOptions>({
     name: 'ping',
-    description: "Shows the bot's latency"
+    description: "Pings the bot",
+    detailedDescription: "Shows the bot's latency, calculated as the time between your command and the bot's response."
 })
 export default class extends Command {
     public async run(message: Message) {
