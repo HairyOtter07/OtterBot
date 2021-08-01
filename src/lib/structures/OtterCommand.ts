2d978fc959499d2ac05d1894304c15c09a7076fb
HairyOtter07 <66044018+HairyOtter07@users.noreply.github.com>
2021-07-31 19:42:18 -0700

    refactor: add property 'fullName' to OtterCommand

--- src/lib/structures/OtterCommand.ts
+++ src/lib/structures/OtterCommand.ts
@@ -5,12 +5,15 @@ export abstract class OtterCommand extends Command {
     public store!: Store<OtterCommand>;
 
     public category: CategoryOption;
+    public fullName: string;
 
     public constructor(context: PieceContext, options: OtterCommandOptions) {
         super(context, options);
 
         const { category } = options;
         this.category = category;
+
+        this.fullName = this.category + "/" + this.name;
     }
 }
 
