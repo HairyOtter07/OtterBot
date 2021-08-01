d39a56e48e145a6aee95d9f0d74158e7cf999d44
HairyOtter07 <66044018+HairyOtter07@users.noreply.github.com>
2021-07-31 19:47:41 -0700

    refactor: add property 'examples' to OtterCommand

--- src/lib/structures/OtterCommand.ts
+++ src/lib/structures/OtterCommand.ts
@@ -5,13 +5,15 @@ export abstract class OtterCommand extends Command {
     public store!: Store<OtterCommand>;
 
     public category: CategoryOption;
+    public examples: string[];
     public fullName: string;
 
     public constructor(context: PieceContext, options: OtterCommandOptions) {
         super(context, options);
 
-        const { category } = options;
+        const { category, examples } = options;
         this.category = category;
+        this.examples = examples;
 
         this.fullName = this.category + "/" + this.name;
     }
@@ -19,4 +21,5 @@ export abstract class OtterCommand extends Command {
 
 export interface OtterCommandOptions extends CommandOptions {
     category: CategoryOption;
+    examples: string[];
 }
