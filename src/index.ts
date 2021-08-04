967fd2e179828aa51af3dd654d15dcaf8be1d854
HairyOtter07 <66044018+HairyOtter07@users.noreply.github.com>
2021-08-03 17:54:47 -0700

    feat: implement client.fetchPrefix

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,13 @@ const client = new OtterClient({
     intents: [ 
         Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.DIRECT_MESSAGES
     ],
-    defaultPrefix: "ob?",
+    defaultPrefix: process.env.PREFIX,
     regexPrefix: /^(hey +)?otterbot[,! ]/i,
     logger: {
         level: LogLevel.Debug
     }
 });
+
+client.fetchPrefix = () => process.env.PREFIX;
 
 client.login(process.env.TOKEN);
