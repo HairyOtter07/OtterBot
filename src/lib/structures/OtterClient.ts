72e74eb1390f0a4ea8076db41fc4037454c718f2
HairyOtter07 <66044018+HairyOtter07@users.noreply.github.com>
2021-08-08 12:08:51 -0700

    chore: add piece load logger

--- src/lib/structures/OtterClient.ts
+++ src/lib/structures/OtterClient.ts
@@ -1,8 +1,10 @@
-import { SapphireClient } from "@sapphire/framework";
+import { container, SapphireClient, Store } from "@sapphire/framework";
 import type { ClientOptions } from "discord.js";
 
 export default class OtterClient extends SapphireClient {
     public constructor(options: ClientOptions) {
         super(options);
+
+        Store.defaultStrategy.onLoad = (store, piece) => container.logger.info(`[INFO] Loading ${store.name}:${piece.name}`);
     }
 }
