264765a7cdf3c4d4ba986079ec4032db0dd36ab6
HairyOtter07 <66044018+HairyOtter07@users.noreply.github.com>
2021-08-15 19:11:49 -0700

    feat: add date formatting func

--- src/lib/util/functions.ts
+++ src/lib/util/functions.ts
@@ -1,5 +1,10 @@
+import { DateTime } from "luxon";
 import { CategoryOption, categoryOptions } from "./constants"
 
 export const isValidCategory = (category: string): category is CategoryOption => {
     return (categoryOptions as ReadonlyArray<string>).includes(category);
+}
+
+export const formatTimestamp = (timestamp: number): string => {
+    return DateTime.fromMillis(timestamp).toLocaleString(DateTime.DATE_HUGE);
 }
